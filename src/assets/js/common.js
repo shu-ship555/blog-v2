06794e0396047cc2150a960d87afbca1b666a7fc
Shu Miyata <ms0079535@gmail.com>
2025-08-10 11:52:21 +0900

    スタイリング

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,25 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const openButton = document.querySelector('.js-open');
-  const closeButton = document.querySelector('.js-close');
-  const menuElement = document.querySelector('.js-menu');
-  const backgroundElement = document.querySelector('.js-bg');
+	const openButton = document.querySelector('.js-open');
+	const closeButton = document.querySelector('.js-close');
+	const menuElement = document.querySelector('.js-menu');
+	const backgroundElement = document.querySelector('.js-bg');
 
-  let scrollPosition = 0;
+	let scrollPosition = 0;
 
-  if (openButton && closeButton && menuElement && backgroundElement) {
-    const openMenu = () => {
-      scrollPosition = window.scrollY;
-      document.body.classList.add('is-active');
-      window.scrollTo(0, 0);
-    };
+	if (openButton && closeButton && menuElement && backgroundElement) {
+		const openMenu = () => {
+			scrollPosition = window.scrollY;
+			document.body.classList.add('is-active');
+			window.scrollTo({
+				top: 0,
+				behavior: 'smooth'
+			});
+		};
 
-    const closeMenu = () => {
-      document.body.classList.remove('is-active');
-      window.scrollTo(0, scrollPosition);
-    };
+		const closeMenu = () => {
+			document.body.classList.remove('is-active');
+			window.scrollTo({
+				top: scrollPosition,
+				behavior: 'smooth'
+			});
+		};
 
-    openButton.addEventListener('click', openMenu);
-    closeButton.addEventListener('click', closeMenu);
-    backgroundElement.addEventListener('click', closeMenu);
-  }
+		openButton.addEventListener('click', openMenu);
+		closeButton.addEventListener('click', closeMenu);
+		backgroundElement.addEventListener('click', closeMenu);
+	}
 });
